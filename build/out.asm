8bee5bdc9574a1ebe60df42f91459d02e7580355
Robert Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-03-24 03:39:13 +0000

    Found a bug

--- build/out.asm
+++ build/out.asm
@@ -50,98 +50,6 @@ __4: ; printi
 	leave
 	ret
 
-_1: ; new
-	push rbp
-	mov rbp, rsp
-	sub rsp, 16
-	; '        return @LL {'
-	; [inline asm]
-	mov rax, qword 0
-	mov qword [rbp-16], rax
-	; [inline asm]
-	mov qword [rbp-8], 1
-	; [inline asm]
-	; [return] Some((-16, 16))
-	; [local copy] -16 , 16, 16
-	mov rax, qword [rbp-16]
-	mov qword [rbp+16], rax
-	mov rax, qword [rbp-8]
-	mov qword [rbp+24], rax
-	leave
-	ret
-
-main: ; main
-	push rbp
-	mov rbp, rsp
-	sub rsp, 64
-	; '    let ll: LL = LL#new();'
-	; [return call] 1 , [], -16
-	sub rsp, 8
-	sub rsp, 8
-	call _1
-	; [local copy] -80 , -16, 16
-	mov rax, qword [rbp-80]
-	mov qword [rbp-16], rax
-	mov rax, qword [rbp-72]
-	mov qword [rbp-8], rax
-	add rsp, 16
-	; ''
-	; '    ll.add(12);'
-	; [inline asm]
-	mov rax, rbp
-	add rax, -16
-	mov qword [rbp-24], rax
-	; [inline asm]
-	mov rax, qword 12
-	mov qword [rbp-32], rax
-	; [no return call] 2 , [(-24, 8), (-32, 8)]
-	sub rsp, 8
-	mov rax, qword [rbp-32]
-	mov qword [rbp-72], rax
-	sub rsp, 8
-	mov rax, qword [rbp-24]
-	mov qword [rbp-80], rax
-	call _2
-	add rsp, 16
-	; ''
-	; '    ll.add(18);'
-	; [inline asm]
-	mov rax, rbp
-	add rax, -16
-	mov qword [rbp-40], rax
-	; [inline asm]
-	mov rax, qword 18
-	mov qword [rbp-48], rax
-	; [no return call] 2 , [(-40, 8), (-48, 8)]
-	sub rsp, 8
-	mov rax, qword [rbp-48]
-	mov qword [rbp-72], rax
-	sub rsp, 8
-	mov rax, qword [rbp-40]
-	mov qword [rbp-80], rax
-	call _2
-	add rsp, 16
-	; ''
-	; '    ll.print();'
-	; [inline asm]
-	mov rax, rbp
-	add rax, -16
-	mov qword [rbp-56], rax
-	; [no return call] 3 , [(-56, 8)]
-	sub rsp, 8
-	mov rax, qword [rbp-56]
-	mov qword [rbp-72], rax
-	call _3
-	add rsp, 8
-	; ''
-	; '    return 7;'
-	; [inline asm]
-	mov rax, qword 7
-	mov qword [rbp-64], rax
-	; [return] Some((-64, 8))
-	mov rcx, qword [rbp-64]
-	call ExitProcess
-
 _3: ; print
 	push rbp
 	mov rbp, rsp
@@ -435,5 +343,108 @@ _2: ; add
 	mov r9, qword [rbp-152]
 	mov rax, qword [rbp-160]
 	mov qword [r9+0], rax
+	leave
+	ret
+
+main: ; main
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16
+	; '    test();'
+	; [no return call] 4 , []
+	call _4
+	add rsp, 0
+	; ''
+	; '    return 7;'
+	; [inline asm]
+	mov rax, qword 7
+	mov qword [rbp-8], rax
+	; [return] Some((-8, 8))
+	mov rcx, qword [rbp-8]
+	call ExitProcess
+
+_4: ; test
+	push rbp
+	mov rbp, rsp
+	sub rsp, 64
+	; '    let ll: LL = LL#new();'
+	; [return call] 1 , [], -16
+	sub rsp, 8
+	sub rsp, 8
+	call _1
+	; [local copy] -80 , -16, 16
+	mov rax, qword [rbp-80]
+	mov qword [rbp-16], rax
+	mov rax, qword [rbp-72]
+	mov qword [rbp-8], rax
+	add rsp, 16
+	; ''
+	; '    ll.add(12);'
+	; [inline asm]
+	mov rax, rbp
+	add rax, -16
+	mov qword [rbp-24], rax
+	; [inline asm]
+	mov rax, qword 12
+	mov qword [rbp-32], rax
+	; [no return call] 2 , [(-24, 8), (-32, 8)]
+	sub rsp, 8
+	mov rax, qword [rbp-32]
+	mov qword [rbp-72], rax
+	sub rsp, 8
+	mov rax, qword [rbp-24]
+	mov qword [rbp-80], rax
+	call _2
+	add rsp, 16
+	; ''
+	; '    ll.add(18);'
+	; [inline asm]
+	mov rax, rbp
+	add rax, -16
+	mov qword [rbp-40], rax
+	; [inline asm]
+	mov rax, qword 18
+	mov qword [rbp-48], rax
+	; [no return call] 2 , [(-40, 8), (-48, 8)]
+	sub rsp, 8
+	mov rax, qword [rbp-48]
+	mov qword [rbp-72], rax
+	sub rsp, 8
+	mov rax, qword [rbp-40]
+	mov qword [rbp-80], rax
+	call _2
+	add rsp, 16
+	; ''
+	; '    ll.print();'
+	; [inline asm]
+	mov rax, rbp
+	add rax, -16
+	mov qword [rbp-56], rax
+	; [no return call] 3 , [(-56, 8)]
+	sub rsp, 8
+	mov rax, qword [rbp-56]
+	mov qword [rbp-72], rax
+	call _3
+	add rsp, 8
+	leave
+	ret
+
+_1: ; new
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16
+	; '        return @LL {'
+	; [inline asm]
+	mov rax, qword 0
+	mov qword [rbp-16], rax
+	; [inline asm]
+	mov qword [rbp-8], 1
+	; [inline asm]
+	; [return] Some((-16, 16))
+	; [local copy] -16 , 16, 16
+	mov rax, qword [rbp-16]
+	mov qword [rbp+16], rax
+	mov rax, qword [rbp-8]
+	mov qword [rbp+24], rax
 	leave
 	ret
